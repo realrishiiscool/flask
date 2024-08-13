0c6052820646a595be9a1be325f25ac065072b6f
ramprasadsuthi <59646329+ramprasadsuthi@users.noreply.github.com>
2024-08-13 20:19:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask,url_for,redirect,render_template
 
 app = Flask(__name__)
 #this  is one lie edioted
+#This is from Ram
 @app.route('/')
 def index():
 	return render_template('index.html')
