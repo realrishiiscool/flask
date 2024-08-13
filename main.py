311473b9499a933760fe809cde30914650264b9c
Rishi <141233915+realrishiiscool@users.noreply.github.com>
2024-08-13 20:19:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask,url_for,redirect,render_template
 
 app = Flask(__name__)
 #this  is one lie edioted
+#this is change 2 in same line
 @app.route('/')
 def index():
 	return render_template('index.html')
